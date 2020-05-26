b0b08b0db1b07e819648b311086b5e19fe5787b6
jsvigneshkanna <42484705+jsvigneshkanna@users.noreply.github.com>
2020-05-26 14:55:11 +0530

    Add files via upload

---
+++ Railway_project/src/com/railway/UiDemo.java
@@ -0,0 +1,21 @@
+package com.railway;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UiDemo extends Frame {
+		UiDemo(){  
+		Button b=new Button("click me");  
+		b.setBounds(30,100,80,30);// setting button position  
+		add(b);//adding button into frame  
+		setSize(300,300);//frame size 300 width and 300 height  
+		setLayout(null);//no layout manager  
+		setVisible(false);//now frame will be visible, by default not visible  
+		}  
+
+	public static void main(String[] args) {
+		
+		UiDemo vk = new UiDemo();
+	}
+
+}
